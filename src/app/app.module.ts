5dc368fa5e103e5f4bc0362ff96a396284383dc5
DavidKibetRono <david.rono@student.moringaschool.com>
2022-01-16 21:05:22 +0300

    add the root module that assembles all the created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HttpClientModule } from '@angular/common/http';
+import { GithubUserDetailsComponent } from './github-user-details/github-user-details.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { MyRoutingModule } from './my-routing/my-routing.module';
+import { TimePassedPipe } from './time-passed.pipe';
+import { HighlightDirective } from './highlight.directive';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    ProfileComponent,
+    GithubUserDetailsComponent,
+    RepositoriesComponent,
+    PageNotFoundComponent,
+    TimePassedPipe,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MyRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
